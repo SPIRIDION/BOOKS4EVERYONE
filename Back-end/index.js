d4b14542e694d04043f1ae4e475c89610d295ea0
SPIRIDION <ca_messy@hotmail.it>
2025-06-24 18:45:57 +0200

    Modifiche per configurazione render

--- Back-end/index.js
+++ Back-end/index.js
@@ -5,12 +5,13 @@ const helmet = require('helmet')
 require('dotenv').config()
 
 const app = express()
-const port = 3001// utilizziamo la porta 3001 perchè la 3000 potrebbe creare interferenze con React
+const port = process.env.PORT || 3001
 const dbName = 'BOOKS4EVERYONE'
 
 // middleware vari
 app.use(cors())// per la gestione dei cors
 app.use(express.json())// per la gestione del formato JSON
+app.use(helmet()) 
 
 // importiamo le rotte per users, offerte e commenti
 app.use('/users', require('./routes/userRoutes'))
@@ -19,5 +20,8 @@ app.use('/comments', require('./routes/commentRoutes'))
 
 // connessione al server e avvio 
 mongoose.connect(process.env.MONGODB_URL + dbName)
-  .then(resp => app.listen(port, () => console.log(`Server attivo sulla porta: ${port}`)))
-  .catch(err => console.error(err))
+  .then(() => {
+    console.log(`Connesso al DB ${dbName}`)
+    app.listen(port, () => console.log(`Server attivo sulla porta: ${port}`))
+  })
+  .catch(err => console.error('Errore nella connessione al DB:', err))
