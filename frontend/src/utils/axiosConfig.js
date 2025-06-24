7450e6bd93030b41d622bacc51ace8e7e0797130
SPIRIDION <ca_messy@hotmail.it>
2025-06-24 19:01:47 +0200

    Modifica configurazione di axios con url di render

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://books4everyone.onrender.com'
 })
 
 instance.interceptors.request.use((config) => {
